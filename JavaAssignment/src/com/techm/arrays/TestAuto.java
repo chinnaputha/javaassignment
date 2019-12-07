5f4669af00e121cf80738d7443cfe4c14c510267
Chinna.Putha <chinnareddy.putha@gmail.com>
2019-12-07 13:50:24 +0530

    Adding file

--- JavaAssignment/src/com/techm/arrays/TestAuto.java
+++ JavaAssignment/src/com/techm/arrays/TestAuto.java
@@ -3,7 +3,7 @@ package com.techm.arrays;
 public class TestAuto {
 
 	public static void main(String[] args) {
-		System.out.println("TestAuto.main()");
+		System.out.println("TestAuto.main()"); 
 
 	}
 
